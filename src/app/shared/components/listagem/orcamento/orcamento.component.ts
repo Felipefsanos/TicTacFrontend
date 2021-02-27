eda5c3c4d079a263e1226f0e42f68a70e1318eef
conta.comercial.tictac <conta.comercial.tictac@gmail.com>
2021-02-27 09:51:32 -0300

    Criação de tabela dinamica

--- src/app/shared/components/listagem/orcamento/orcamento.component.ts
+++ src/app/shared/components/listagem/orcamento/orcamento.component.ts
@@ -3,15 +3,27 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { OrcamentoService } from 'src/app/services/orcamento.service';
 import { MessageService } from 'src/app/shared/services/message.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
  
+ 
+
 @Component({
   selector: 'app-orcamento',
   templateUrl: './orcamento.component.html',
-  styleUrls: ['./orcamento.component.scss']
+  styleUrls: ['./orcamento.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class OrcamentoComponent implements OnInit {
-  displayedColumns: string[] = ['dataEvento', 'tipoEvento', 'buffetPrincipal', 'observacao','acao'];
+  columnsToDisplay : string[] = ['id','dataEvento', 'tipoEvento', 'buffetPrincipal', 'observacao'];
+  columnsToName : string[] = ['id','dataEvento', 'tipoEvento', 'buffetPrincipal', 'observacao'];
   dataSource = new MatTableDataSource<OrcamentoModel>();
+  expandedElement: any | null;
   
   constructor(  private orcamentoService: OrcamentoService,
     private messageService: MessageService) { }
@@ -21,6 +33,12 @@ export class OrcamentoComponent implements OnInit {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
+  editar(id: number): void {
+
+  }
+  excluir(id: number): void {
+
+  }
 
   ngOnInit() {
     debugger;
