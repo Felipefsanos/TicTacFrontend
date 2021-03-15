99bdc2e1724c22f216b2a3c546fd65106b76575d
conta.comercial.tictac <conta.comercial.tictac@gmail.com>
2021-03-15 17:09:15 -0300

    ajustes de telas de produto

--- src/app/shared/components/formularios/produto-formulario/produto-formulario.component.ts
+++ src/app/shared/components/formularios/produto-formulario/produto-formulario.component.ts
@@ -1,5 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProdutoService } from 'src/app/services/produto.service';
+import { SubProdutoService } from 'src/app/services/sub-produto.service';
+import { MessageService } from 'src/app/shared/services/message.service';
 
 @Component({
   selector: 'app-produto-formulario',
@@ -8,46 +12,62 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 })
 export class ProdutoFormularioComponent implements OnInit {
 
+  subProdutosList: any | undefined = [{text: 'Selecione...', valor: 0}];
   produtoForm: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private produtoService: ProdutoService, 
+    private subProdutoService: SubProdutoService,
+    private messageService: MessageService) {
     this.construirFormularioInformacoesCliente();
   }
 
   construirFormularioInformacoesCliente() {
     this.produtoForm = this.formBuilder.group({
-      nome: [''],
-      descricao: [''],
-      valor: [''],
-      subProdutos: this.formBuilder.array([
-        this.formBuilder.group({
-          nome: ['', Validators.required],
-          descricao: [''],
-          valor: ['', Validators.email]
-        })
-      ]),
+      nome: ['',Validators.required],
+      descricao: ['',Validators.required],
+      valor: ['',Validators.required],
+      subprodutos:  ['']
     });
   }
 
   ngOnInit(): void {
+    this.obterSubProdutos(0);
   }
-  onSubmit(): void {
-    console.log(this.produtoForm);
-  }
+ 
 
   getErrorMessageSubProdutos(formGroupIndex: number, controlName: string): FormControl {
     const formGroup = this.subProdutos.controls[formGroupIndex] as FormGroup;
     return formGroup.get(controlName) as FormControl;
   }
 
+  obterSubProdutos(codigoProduto: number): void {
+    debugger;
+    if(codigoProduto <= 0)
+    {
+      this.subProdutoService.obterSubProdutos().subscribe(
+        res=> {
+          this.subProdutosList = [];
+          for( const item of res){
+           this.subProdutosList.push([{text: item.nome ,valor: item.id}]);
+          }
+        },(error: HttpErrorResponse) => {
+            this.messageService.warn("Erro para acessar service:" + error.error.menssage)
+        })
+    }else{
+      this.subProdutoService.obterSubProduto(codigoProduto).subscribe(
+        resp => {
+          this.subProdutosList = [];
+          for( const item of resp){
+           this.subProdutosList.push([{text: item.nome ,valor: item.id}]);
+          }
+        },(error: HttpErrorResponse) => {
+          this.messageService.warn("Erro para acessar service:" + error.error.menssage)
+      })
+    }
+  }
 
-  addCreds() {
-    const creds = this.produtoForm.controls.subProdutos as FormArray;
-    creds.push(this.formBuilder.group({
-      nome: ['', Validators.required],
-      descricao: [''],
-      valor: ['', Validators.email]
-    }));
+  onSubmit(): void {
+    console.log(this.produtoForm);
   }
 
   get nome(): FormControl {
